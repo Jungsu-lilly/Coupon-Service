65df3b8d547ce1926822a44e4cdb45b33c8ad8df
matt <matt1235@naver.com>
2024-04-18 01:00:56 +0900

    refactor: 쿠폰 발급 서비스 로직 리팩토링 (#5)

--- coupon-core/src/main/java/com/example/couponcore/service/RedisCouponIssueService.java
+++ coupon-core/src/main/java/com/example/couponcore/service/RedisCouponIssueService.java
@@ -1,6 +1,9 @@
 package com.example.couponcore.service;
 
+import com.example.couponcore.exception.common.BaseException;
+import com.example.couponcore.exception.common.ConflictException;
 import com.example.couponcore.repository.redis.RedisRepository;
+import com.example.couponcore.repository.redis.dto.CouponRedisEntity;
 import com.example.couponcore.util.CouponRedisUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,15 @@ import org.springframework.stereotype.Service;
 public class RedisCouponIssueService {
 
     private final RedisRepository redisRepository;
+
+    public void checkCouponIssueQuantity(CouponRedisEntity coupon, long userId) {
+        if (!availableUserIssueQuantity(coupon.id(), userId)) {
+            throw new ConflictException("이미 발급된 쿠폰입니다.");
+        }
+        if (!availableTotalIssueQuantity(coupon.totalQuantity(), coupon.id())) {
+            throw new BaseException(400, "발급 가능한 수량을 초과합니다. couponId : %s, userId : %s".formatted(coupon.id(), userId));
+        }
+    }
 
     public boolean availableTotalIssueQuantity(Integer totalQuantity, long couponId) {
         if (totalQuantity == null) {
