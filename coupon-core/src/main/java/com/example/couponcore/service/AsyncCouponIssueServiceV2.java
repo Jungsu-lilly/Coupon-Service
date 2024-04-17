a39a7b105d7c4c4d4643b44e2e37fdbb61eca917
matt <matt1235@naver.com>
2024-04-18 02:44:56 +0900

    test: async-v2 테스트 코드 작성 (#5)

--- coupon-core/src/main/java/com/example/couponcore/service/AsyncCouponIssueServiceV2.java
+++ coupon-core/src/main/java/com/example/couponcore/service/AsyncCouponIssueServiceV2.java
@@ -1,9 +1,7 @@
 package com.example.couponcore.service;
 
-import com.example.couponcore.component.DistributeLockExecutor;
 import com.example.couponcore.repository.redis.RedisRepository;
 import com.example.couponcore.repository.redis.dto.CouponRedisEntity;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,18 +10,18 @@ import org.springframework.stereotype.Service;
 public class AsyncCouponIssueServiceV2 {
 
     private final RedisRepository redisRepository;
-    private final RedisCouponIssueService redisCouponIssueService;
-    private final DistributeLockExecutor distributeLockExecutor;
     private final CouponCacheService couponCacheService;
-    private ObjectMapper objectMapper = new ObjectMapper();
 
     public void issue(long couponId, long userId) {
         CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
         coupon.checkIssuableCoupon();
-        issueRequest(couponId, userId);
+        issueRequest(couponId, userId, coupon.totalQuantity());
     }
 
-    private void issueRequest(long couponId, long userId) {
-
+    private void issueRequest(long couponId, long userId, Integer totalIssueQuantity) {
+        if (totalIssueQuantity == null) {
+            redisRepository.issueRequest(couponId, userId, Integer.MAX_VALUE);
+        }
+        redisRepository.issueRequest(couponId, userId, totalIssueQuantity);
     }
 }
