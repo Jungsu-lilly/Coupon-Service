88b0f21ead86fcc983e2e190be3eb8e2f1a5f559
matt <matt1235@naver.com>
2024-05-08 04:25:55 +0900

    refactor: 쿠폰 Controller, Service 리팩토링 (#7)

--- coupon-core/src/main/java/com/example/couponcore/service/AsyncCouponIssueServiceV1.java
+++ coupon-core/src/main/java/com/example/couponcore/service/AsyncCouponIssueServiceV1.java
@@ -1,6 +1,5 @@
 package com.example.couponcore.service;
 
-import com.example.couponcore.component.DistributeLockExecutor;
 import com.example.couponcore.exception.common.BaseException;
 import com.example.couponcore.repository.redis.RedisRepository;
 import com.example.couponcore.repository.redis.dto.CouponIssueRequest;
@@ -19,25 +18,22 @@ public class AsyncCouponIssueServiceV1 {
 
     private final RedisRepository redisRepository;
     private final RedisCouponIssueService redisCouponIssueService;
-    private final DistributeLockExecutor distributeLockExecutor;
     private final CouponCacheService couponCacheService;
     private ObjectMapper objectMapper = new ObjectMapper();
 
     public void issue(long couponId, long userId) {
-        CouponRedisEntity coupon = couponCacheService.getCouponCache(couponId);
-        coupon.checkIssuableCoupon();
-        distributeLockExecutor.execute("lock_%s".formatted(couponId), 3000, 3000, () -> {
-            redisCouponIssueService.checkCouponIssueQuantity(coupon, userId);
-            issueRequest(couponId, userId);
-        });
+        CouponRedisEntity coupon = couponCacheService.getCachedCoupon(couponId);
+
+        redisCouponIssueService.verifyCouponIssuance(coupon, userId);
+        issueCoupon(couponId, userId);
     }
 
-    private void issueRequest(long couponId, long userId) {
+    private void issueCoupon(long couponId, long userId) {
         CouponIssueRequest issueRequest = new CouponIssueRequest(couponId, userId);
         try {
             String value = objectMapper.writeValueAsString(issueRequest);
-            redisRepository.sAdd(getIssueRequestKey(couponId), String.valueOf(userId));
             redisRepository.rPush(getQueueKey(), value);
+            redisRepository.sAdd(getIssueRequestKey(couponId), String.valueOf(userId));
         } catch (JsonProcessingException e) {
             throw new BaseException(400, "쿠폰 발급 실패");
         }
