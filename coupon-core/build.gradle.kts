cef58c39c8c612f187398486360bee9a57935146
matt <matt1235@naver.com>
2024-05-09 16:34:21 +0900

    build: 로컬 캐시를 위한 의존성 추가 (#11)

--- coupon-core/build.gradle.kts
+++ coupon-core/build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.redisson:redisson-spring-boot-starter:3.16.4")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // 로컬 캐시 적용 : 카페인
+    implementation("com.github.ben-manes.caffeine:caffeine")
 }
 
 tasks.withType<Test> {
